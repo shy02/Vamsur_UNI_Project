5143c043933f74d3864782889a700fa88ac30602
msh0325 <msh0325@daum.net>
2023-09-19 15:23:36 +0900

    Add_Enemy_AI
    Enemy_Scenes을 만들고, 몹이 플레이어를 따라가는 스크립트를 짰습니다.
    
    Co-Authored-By: msh0325 <113653680+msh0325@users.noreply.github.com>

---
+++ Vampire_Survival_Like/Assets/Script/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Rigidbody2D target;
+    bool isLive;
+
+    SpriteRenderer spriter;
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+    //FixedUpdate는 물리엔진 안고장나게 쓰는 Update.
+    void FixedUpdate()
+    {
+        //타겟 포지션 - 몹 포지션 = 플레이어로 향하는 방향
+        Vector2 director = target.position - rigid.position;
+
+        //방향 * 몹스피드 * 시간 = 다음에 갈 위치
+        Vector2 next = director.normalized * speed * Time.fixedDeltaTime;
+
+        //다음에 가야할 위치로 움직이기
+        rigid.MovePosition(rigid.position + next);
+
+        //물리적인 속도를 없애 무브포지션과의 충돌 없애기
+        rigid.velocity = Vector2.zero;
+
+    }
+    //LateUpdate는 Update에 있는 함수가 모두 끝난 뒤에 실행.
+    private void LateUpdate()
+    {
+        //타겟의 x위치가 몹의 x위치보다 오른쪽에 있을 때 스프라이트 뒤집기.
+        spriter.flipX = target.position.x > rigid.position.x;
+    }
+}
