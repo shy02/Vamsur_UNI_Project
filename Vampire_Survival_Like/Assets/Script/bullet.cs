598588f47c7daecbffdd296e82930bb01bca9814
Gyeong_Min <104749073+J0Min@users.noreply.github.com>
2023-09-24 01:35:50 +0900

    ADD_Longdistance_Enermy
    원거리 공격하는 적 초기단계 입니다

---
+++ Vampire_Survival_Like/Assets/Script/bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    public LayerMask isLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyBullet", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D raycast = Physics2D.Raycast(transform.position, transform.right * -1f , distance, isLayer);
+
+        if (raycast.collider != null)
+        {
+            if(raycast.collider.tag == "Player")
+            Debug.Log("∏∂¡Ú");
+        DestroyBullet();
+        }
+        
+        transform.Translate(transform.right * -1f * speed * Time.deltaTime);
+    }
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+        Debug.Log(transform.right + this.gameObject.name);
+    }
+}
