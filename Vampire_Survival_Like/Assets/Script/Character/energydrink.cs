c621b332e327e8e9dc50b3ef792d58688e755452
hanraeul <112698340+hanraeul@users.noreply.github.com>
2023-10-27 12:28:27 +0900

    스킬추가+
    에너지드링크 스킬 추가 (데미지가 안들어감 추후 수정)

---
+++ Vampire_Survival_Like/Assets/Script/Character/energydrink.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class energydrink : MonoBehaviour
+{
+    public int id;
+    public int prefabId;
+    public float damage;
+    
+
+
+    void Start()
+    {
+        Init();
+    }
+
+    
+    public void Init()
+    {
+        Batch();
+    }
+    void Batch()
+    {
+        
+            Transform bullet =GameManager.instance.Player_pool.Get(prefabId).transform;
+            bullet.parent = transform;
+             
+            bullet.GetComponent<Skill>().Init(damage, -1); // -1 is infinity per. 무한 공
+
+        
+    }
+}
