fba0af02d6c9383b6f08b7d890de52d0389af37d
msh0325 <msh0325@daum.net>
2023-11-01 19:33:39 +0900

    Add_Snake_Boss_Pattern
    뱀 보스의 패턴(독안개)를 추가하였습니다. 독안개, 뱀꼬리 이미지(임시)를 추가하였습니다.

--- Vampire_Survival_Like/Assets/Script/Enemy/Boss_Snake.cs
+++ Vampire_Survival_Like/Assets/Script/Enemy/Boss_Snake.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Boss_Snake : MonoBehaviour
 {
-    Animator animator;
 
     Rigidbody2D target;
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
 
     public GameObject tail;
+    public GameObject snake_fog;
 
     public int boss_HP; //보스 초기 체력
     public int current_boss_HP; // 보스 현재 체력
@@ -27,12 +28,10 @@ public class Boss_Snake : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //애니메이션
-        animator = GetComponent<Animator>();
-        animator.SetBool("isfirst", false);
-
+        spriter = GetComponent<SpriteRenderer>();
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         rigid = GetComponent<Rigidbody2D>();
+        snake_fog.SetActive(false);
     }
 
     // Update is called once per frame
@@ -47,6 +46,7 @@ public class Boss_Snake : MonoBehaviour
             if (fog_time > 3)
             {
                 fog_area = false;
+                snake_fog.SetActive(false);
                 fog_time = 0;
             }
         }
@@ -55,7 +55,10 @@ public class Boss_Snake : MonoBehaviour
         Vector2 director = target.position - rigid.position;
         Vector2 next = director.normalized * speed * Time.deltaTime;
         //보스 이동
-        rigid.MovePosition(rigid.position + next);
+        if (Vector3.Distance(transform.position, target.position) > 10)
+        {
+            rigid.MovePosition(rigid.position + next);
+        }
         //공격 변수
         random_ = Random.Range(1, 4);
         //꼬리 위치
@@ -74,10 +77,15 @@ public class Boss_Snake : MonoBehaviour
                     break;
                 case 3:
                     //독안개
+                    snake_fog.SetActive(true);
                     Debug.Log("FoG");
                     fog_area = true;
                     break;
             }
         }
+    }
+    private void LateUpdate()
+    {
+        spriter.flipX = target.position.x < rigid.position.x;
     }
 }
