4cc4cdfb40cd6736dbb38081222dbc002e9448de
Gyeong_Min <104749073+J0Min@users.noreply.github.com>
2023-11-06 22:34:00 +0900

    fix 4boss
    근접 원거리 소환 숨기

--- Vampire_Survival_Like/Assets/Script/Enemy/ATK_area_event.cs
+++ Vampire_Survival_Like/Assets/Script/Enemy/ATK_area_event.cs
@@ -7,10 +7,11 @@ public class ATK_area_event : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(transform.name);
-        //데미지 함수 필요
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(transform.name);
+        //데미지 함수 필요
     }
 }
--- Vampire_Survival_Like/Assets/Script/Enemy/Boss_4.cs
+++ Vampire_Survival_Like/Assets/Script/Enemy/Boss_4.cs
@@ -9,10 +9,24 @@ public class Boss_4 : MonoBehaviour
     SpriteRenderer spriter;
     Rigidbody2D rigid;
 
-    float timer = 0;
-    float speed = 5;
+    public BoxCollider2D area; // 근접 공격 범위
+    public Transform[] L_Point; // 쫄몹 생성위치
+    public GameObject spawnMob;
+    public GameObject bullet;
+    public GameObject fog;
 
-    bool is_fog;
+
+
+    public float fog_time = 0;
+    public float cool_time;
+    float speed = 1;
+    public int fog_count = 0;
+    public int random_;
+    public float distance;
+    public int i_distance;
+
+    public bool is_fog;
+    public bool rnd_use;
 
     // Start is called before the first frame update
     void Start()
@@ -20,44 +34,137 @@ public class Boss_4 : MonoBehaviour
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
-        is_fog = true;
+        is_fog = false;
+        rnd_use = true;
+        fog.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if(is_fog == false)
+        //공격 쿨타임 조정
+        distance = Vector3.Distance(transform.position, target.position);
+        i_distance = (int)distance;
+        //공격 패턴 랜덤 함수
+        if (rnd_use == true)
         {
-
-            Vector2 director = target.position - rigid.position;
-            Vector2 next = director.normalized * speed * Time.deltaTime;
-
-            float distance = Vector3.Distance(transform.position, target.position);
-
-
-            if (distance < 1) // 1보다 가까울때
-            {
-
-            }
-            else if (distance >= 1 && distance<4) // 1~4 사이일때 빠른 접근후 근접공격 (약 1.2배속)
-            {
-                rigid.MovePosition(rigid.position + next * 1.2f);
-            }
-            else if(distance >= 4) // 4보다 멀때 원거리 공격
-            {
-                rigid.MovePosition(rigid.position + next);
-            }
+            random_ = Random.Range(1, 3);
+            rnd_use = false;
         }
+        // 연막탄 공격을 하지 않고 쿨타임이 2초 지났을때
+        if (is_fog == false)
+        {
+            cool_time += Time.deltaTime;
+            if (fog_count < 5)
+            {
+                switch (random_)
+                {
+                    case 1:
+                        //근접 공격
+                        if (cool_time > 5)
+                        {
+                            Debug.Log("근접 성공");
+                            fog_count += 1;
+                            rnd_use = true;
+                            StopCoroutine("atk_area"); //코루틴 함수로 공격범위 제어
+                            StartCoroutine("atk_area");
+                            cool_time = 0;
+                            break;
+                        }
+                        if (distance < 0) //너무 근접했을때 도망
+                        {
+                            moving(-1);
+                        }
+                        else if (distance > 2) // 거리가 멀어서 접근
+                        {
+                            Debug.Log("근접 실패");
+                            if (cool_time < 5)
+                            {
+                                moving(1.2f);
+                            }
+                            else // 거리가 멀어서 빠른 접근
+                            {
+                                moving(1.4f);
+                            }
+                        }
+                        else if(i_distance == 1)
+                        {
+                            Debug.Log("근접 실패 - 거리 맞음");
+                        }
+                        break;
+                    case 2:
+                        //원거리 공격
+                        if (cool_time > 5)
+                        {
+                            Debug.Log("원거리 성공");
+                            fog_count += 1;
+                            rnd_use = true;
+                            cool_time = 0;
+                            Instantiate(bullet, rigid.position, Quaternion.identity);
+                            break;
+                        }
+                        if (distance < 5) //너무 근접해서 도망
+                        {
+                            Debug.Log("원거리 실패");
+                            moving(-1);
+                        }
+                        else if (distance > 6) //멀어서 근접
+                        {
+                            moving(1);
+                        }
+                        else if (i_distance == 5)
+                        {
+                            Debug.Log("원거리 실패 - 거리 맞음");
+                        }
+                        break;
 
-        else if(is_fog == true && timer<5)
+                }
+            }
+            else if (fog_count >= 5)
+            {
+                //특수 기믹
+                fog.SetActive(true);
+                fog_count = 0;
+                is_fog = true;
+                Instantiate(spawnMob, L_Point[0].position, Quaternion.identity);
+                Instantiate(spawnMob, L_Point[1].position, Quaternion.identity);
+            }
+
+        }
+        else if (is_fog == true && fog_time < 5)
         {
             spriter.enabled = false;
-            timer += Time.deltaTime;
+            fog_time += Time.deltaTime;
+            //애니메이션도 꺼야함 ㅇㅇ
         }
-        else if(is_fog == true && timer > 5)
+        else if (is_fog == true && fog_time > 5)
         {
-                spriter.enabled = true;
+            is_fog = false;
+            spriter.enabled = true;
+            fog_time = 0;
+            fog.SetActive(false);
         }
     }
+
+    private void moving(float value)
+    {
+        //이동 위치값 조정
+        Vector2 director = target.position - rigid.position;
+        Vector2 next = director.normalized * speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + next * value);
+        transform.GetChild(3).localPosition = director.normalized;
+        Debug.Log("move");
+    }
+
+    IEnumerator atk_area()
+    {
+        area.enabled = true; // 공격범위 활성화
+        yield return new WaitForSeconds(2f); // 2초후 비활성화
+        area.enabled = false;
+    }
+    private void LateUpdate()
+    {
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
 }
+
